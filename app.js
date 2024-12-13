6247814f325af33e5881b496ae8ced855e43185c
AnnaSergeevnaIv <kara4eva.anya@gmail.com>
2024-12-13 11:19:31 +0300

    refactor(basic-3.1,3.2,3.3, extended-2.1): rename classes and id according to BEM

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput = document.getElementById("new-task");//Add a new task.
 var addButton = document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder = document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder = document.getElementById("incompleted-tasks");//ul of #incompleted-tasks
 var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -41,9 +41,9 @@ var createNewTaskElement = function(taskString){
   editInput.className = "task";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "edit";
+  editButton.className = "borderless-button_edit";
 
-  deleteButton.className = "delete";
+  deleteButton.className = "bordered-button";
   deleteButtonImg.src = './remove.svg';
   deleteButton.appendChild(deleteButtonImg);
 
@@ -84,12 +84,12 @@ var editTask = function() {
 
   var editInput = listItem.querySelector('input[type=text]');
   var label = listItem.querySelector("label");
-  var editBtn = listItem.querySelector(".edit");
-  var containsClass = listItem.classList.contains("editMode");
-  //If class of the parent is .editmode
+  var editBtn = listItem.querySelector(".borderless-button_edit");
+  var containsClass = listItem.classList.contains("task_editable");
+  //If class of the parent is .task_editable
   if(containsClass) {
 
-    //switch to .editmode
+    //switch to .task_editable
     //label becomes the inputs value.
     label.innerText = editInput.value;
     editBtn.innerText = "Edit";
@@ -98,8 +98,8 @@ var editTask = function() {
     editBtn.innerText = "Save";
   }
 
-  //toggle .editmode on the parent.
-  listItem.classList.toggle("editMode");
+  //toggle .task_editable on the parent.
+  listItem.classList.toggle("task_editable");
 };
 
 
@@ -155,8 +155,8 @@ var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
   console.log("bind list item events");
   //select ListItems children
   var checkBox = taskListItem.querySelector("input[type=checkbox]");
-  var editButton = taskListItem.querySelector("button.edit");
-  var deleteButton = taskListItem.querySelector("button.delete");
+  var editButton = taskListItem.querySelector("button.borderless-button_edit");
+  var deleteButton = taskListItem.querySelector(".bordered-button");
 
 
   //Bind editTask to edit button.
